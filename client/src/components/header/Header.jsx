085e034e06ae1fadaba05eec5e6be339062e16d4
Divya <singhdiv92@gmail.com>
2022-01-17 14:31:52 +0530

    Initial Commit

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitle-sm">React & Node</span>
+        <span className="headerTitle-lg">Blog</span>
+      </div>
+      <img
+        className="headerImage"
+        src="https://ivetriedthat-sk8e25wd6p31sxyff5d.netdna-ssl.com/wp-content/uploads/2017/03/get-paid-to-listen-to-music.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
+//https://cdn.wallpapersafari.com/57/28/um0X8h.jpg
---
+++ client/src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,106 @@
+import axios from "axios";
+import { useEffect, useState, useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./singlePost.css";
+import { Context } from "../../context/Context";
+
+export default function SinglePost() {
+  const location = useLocation();
+  const path = location.pathname.split("/")[2];
+  const [post, setPost] = useState({});
+  const PF = "http://localhost:5000/images/";
+  const { user } = useContext(Context);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [updateMode, setUpdatemode] = useState(false);
+
+  useEffect(() => {
+    const getPost = async () => {
+      const res = await axios.get("/posts/" + path);
+      setPost(res.data);
+      setTitle(res.data.title);
+      setDesc(res.data.desc);
+    };
+    getPost();
+  }, [path]);
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${post._id}`, {
+        data: { username: user.username },
+      });
+      window.location.replace("/");
+    } catch (err) {}
+  };
+  const handleUpdate = async () => {
+    try {
+      await axios.put(`/posts/${post._id}`, {
+        username: user.username,
+        title,
+        desc,
+      });
+      setUpdatemode(false);
+    } catch (err) {}
+  };
+  return (
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        {post.photo && (
+          <img className="singlePostImg" src={PF + post.photo} alt=""></img>
+        )}
+        {updateMode ? (
+          <input
+            value={title}
+            className="singlePostTitleInput"
+            autoFocus
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        ) : (
+          <h1 className="singlePostTitle">
+            {title}
+            {post.username === user?.username && (
+              <div className="singlePostEdit">
+                <i
+                  className="singlePostIcon fas fa-edit"
+                  onClick={() => setUpdatemode(true)}
+                ></i>
+                <i
+                  className="singlePostIcon far fa-trash-alt"
+                  onClick={handleDelete}
+                ></i>
+              </div>
+            )}
+          </h1>
+        )}
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">
+            Author :
+            <Link to={`/?user=${post.username}`} className="link">
+              {" "}
+              <b>{post.username}</b>
+            </Link>
+          </span>
+          <span className="singlePostDate">
+            {" "}
+            {new Date(post.createdAt).toDateString()}
+          </span>
+        </div>
+        {updateMode ? (
+          <textarea
+            rows={5}
+            value={desc}
+            className="singlePostDescInput"
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        ) : (
+          <p className="singlePostDesc">{desc}</p>
+        )}
+        {updateMode && (
+          <button className="singlePostButton" onClick={handleUpdate}>
+            Update
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
